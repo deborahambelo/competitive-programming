cdff2d0b3af35cc529247e26847875e70d962360
deborahambelo <101412212+deborahambelo@users.noreply.github.com>
2024-03-01 19:48:01 +0530

    Time: 746 ms (69.51%), Space: 23.9 MB (78.41%) - LeetHub

--- 1887-reduction-operations-to-make-the-array-elements-equal/1887-reduction-operations-to-make-the-array-elements-equal.py
+++ 1887-reduction-operations-to-make-the-array-elements-equal/1887-reduction-operations-to-make-the-array-elements-equal.py
@@ -1,10 +1,8 @@
-from typing import List
-
 class Solution:
     def reductionOperations(self, nums: List[int]) -> int:
-        nums.sort(reverse=True)  # Sort the list in descending order
+        nums.sort(reverse=True)
         operations = 0
         for i in range(1, len(nums)):
-            if nums[i] < nums[i - 1]:  # If the current element is smaller than the previous one
-                operations += i  # Increment the operations count by the current index
+            if nums[i] < nums[i - 1]:
+                operations += i
         return operations
