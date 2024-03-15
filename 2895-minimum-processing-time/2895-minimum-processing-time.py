956d965a06868f57250b419159eddfbe441c6ce3
deborahambelo <101412212+deborahambelo@users.noreply.github.com>
2024-03-15 14:30:17 +0530

    Time: 646 ms (6.72%), Space: 32.9 MB (51.32%) - LeetHub

--- 2895-minimum-processing-time/2895-minimum-processing-time.py
+++ 2895-minimum-processing-time/2895-minimum-processing-time.py
@@ -1,20 +1,3 @@
-# class Solution(object):
-#     def minProcessingTime(self, processorTime, tasks):
-#         tasks.sort(reverse = True)
-#         processorTime.sort()
-#         bs = len(tasks) // len(processorTime)
-#         subarrays = [] 
-#         for i in range(0, len(tasks), bs):
-#             sub = tasks[i:i+bs]
-#             subarrays.append(sub)
-        
-        
-#         current_max = float('-inf')
-#         for i in range(len(subarrays)):
-#             for j in range(len(processorTime)):
-#                 sum1 = subarrays[i][0] + processorTime[i]
-#                 current_max = max(current_max,sum1)
-#         return current_max
 class Solution(object):
     def minProcessingTime(self, processorTime, tasks):
         tasks.sort(reverse=True)
@@ -27,8 +10,8 @@ class Solution(object):
 
         current_max = float('-inf')
         for i in range(len(subarrays)):
-            for j in range(len(subarrays[i])):  # Iterate over subarrays, not processorTime
-                sum1 = subarrays[i][j] + processorTime[i]  # Use correct index for processorTime
+            for j in range(len(subarrays[i])):  
+                sum1 = subarrays[i][j] + processorTime[i]
                 current_max = max(current_max, sum1)
         return current_max
 
