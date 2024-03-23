3109e42847b28accca33deb7dc2d8d169add1e66
deborahambelo <101412212+deborahambelo@users.noreply.github.com>
2024-03-23 15:59:02 +0530

    Time: 537 ms (24.47%), Space: 22.7 MB (29.79%) - LeetHub

--- 1839-longest-substring-of-all-vowels-in-order/1839-longest-substring-of-all-vowels-in-order.py
+++ 1839-longest-substring-of-all-vowels-in-order/1839-longest-substring-of-all-vowels-in-order.py
@@ -1,18 +1,18 @@
 class Solution:
     def longestBeautifulSubstring(self, word: str) -> int:
-        vowel_sequence = {'a', 'e', 'i', 'o', 'u'}
-        r = l = 0
+        vowels = {'a', 'e', 'i', 'o', 'u'}
+        r, l = 0, 0
         max_length = 0
-        seen_vowels = set()
+        sub_arr = set()
         
         while r < len(word):
             if r > 0 and word[r] < word[r - 1]:
                 l = r
-                seen_vowels.clear()
+                sub_arr.clear()
+                
+            sub_arr.add(word[r])
             
-            seen_vowels.add(word[r])
-            
-            if len(seen_vowels) == 5:
+            if len(sub_arr) == 5:
                 max_length = max(max_length, r - l + 1)
             
             r += 1
