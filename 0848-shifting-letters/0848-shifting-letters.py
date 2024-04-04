e91885a67ecc6dea84d062ca2f43fe273b3b9a37
deborahambelo <101412212+deborahambelo@users.noreply.github.com>
2024-04-04 12:27:24 +0530

    Time: 707 ms (13.12%), Space: 30 MB (66.88%) - LeetHub

--- 0848-shifting-letters/0848-shifting-letters.py
+++ 0848-shifting-letters/0848-shifting-letters.py
@@ -10,13 +10,12 @@ class Solution:
         suffix_sum = 0  
         
         for i in range(n):
-            suffix_sum += shifts[i]  # Update suffix sum with current shift value
-            suffix[n - 1 - i] = suffix_sum  # Assign cumulative sum to suffix array in reverse order
+            suffix_sum += shifts[i]  
+            suffix[n - 1 - i] = suffix_sum  
         
-        # Shift characters and handle wrapping around 'z'
         for i in range(len(suffix)):
-            # Calculate the shifted ASCII value
             shifted_ascii = (arr[i] - ord('a') + suffix[i]) % 26 + ord('a')
+            print(shifted_ascii)
             arr2[i] = shifted_ascii
         
         arr2_str = ''.join(chr(num) for num in arr2)
