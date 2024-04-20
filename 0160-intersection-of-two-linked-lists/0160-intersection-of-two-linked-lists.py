b6c256d481b44c1683b092882ad3f5facc6fdd6a
deborahambelo <101412212+deborahambelo@users.noreply.github.com>
2024-04-20 10:25:25 +0530

    Time: 72 ms (92.80%), Space: 27.1 MB (74.12%) - LeetHub

--- 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.py
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.py
@@ -6,7 +6,7 @@ class ListNode:
 
 class Solution:
     def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> Optional[ListNode]:
-        # Function to find the length of a linked list
+        
         def get_length(node):
             length = 0
             while node:
@@ -14,18 +14,18 @@ class Solution:
                 node = node.next
             return length
         
-        # Get the lengths of both linked lists
+        
         len_A = get_length(headA)
         len_B = get_length(headB)
         
-        # Initialize pointers at the heads of both lists
+        
         pointer_A = headA
         pointer_B = headB
         
-        # Calculate the difference in lengths between the two lists
+
         length_diff = abs(len_A - len_B)
         
-        # Move the pointer of the longer list forward by the difference
+      
         if len_A > len_B:
             for _ in range(length_diff):
                 pointer_A = pointer_A.next
@@ -33,12 +33,12 @@ class Solution:
             for _ in range(length_diff):
                 pointer_B = pointer_B.next
         
-        # Move both pointers simultaneously until they meet or reach the end of the lists
+
         while pointer_A and pointer_B:
             if pointer_A == pointer_B:
-                return pointer_A  # Intersection point found
+                return pointer_A  
             pointer_A = pointer_A.next
             pointer_B = pointer_B.next
         
-        # If no intersection point is found
+        
         return None
